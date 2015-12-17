49453be1c3344c53805f80b98c9573e02921ff6b
Yoli Meydan <meydany@bxscience.edu>
2015-12-16 21:00:17 -0500

    Another failed attempt

--- FlightAttendantApp/ViewController.swift
+++ FlightAttendantApp/ViewController.swift
@@ -25,30 +25,31 @@ class ViewController: UIViewController {
 //                print(json["origin"])
 //                print(json["headers"]["Host"])
 //        }
-//                
-//        let user = "user"
-//        let password = "password"
-//        
-//        Alamofire.request(.GET, "https://httpbin.org/basic-auth/\(user)/\(password)")
-//            .authenticate(user: user, password: password)
-//            .responseJSON { response in
-//                debugPrint(response)
-//        }
+        let parameters = "utf8=&#x2713;&authenticity_token=+xj+gu4X413A5cxkMXiLq6291XgdZQzIT1ZoaUdcAqN7ReOQ4CJ/ab9n/7HavBDMnC2JLlet++yD4zZ/EzXSPg==&user[username]=34234&user[password]=abcdfs"
         
-        Alamofire.request(.GET, "https://google.com")
-            .responseString { response in
-                print("Success: \(response.result.isSuccess)")
-                print("Response String: \(response.result.value)")
-                self.createFile(response.result.value!, fileName: "GoogleHTML")
+        let URL = NSURL(string: "https://auth.casenex.com/users/sign_in")
+        let mutableURLRequest = NSMutableURLRequest(URL: URL!)
+        mutableURLRequest.HTTPMethod = "POST"
+        
+        do {
+            mutableURLRequest.HTTPBody = parameters.dataUsingEncoding(NSUTF8StringEncoding)
+        } catch {
+            // No-op
         }
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(mutableURLRequest)
+            .responseString { response in
+                print(response.result)
+                print("Response String: \(response.result.value)")
+                self.createFile(response.result.value, fileName: "ResponseString")
+        }
     }
     
-    func createFile(textToWrite: String!, fileName: String!) -> Void {        
+    func createFile(textToWrite: String!, fileName: String!) -> Void {
         let text = textToWrite //just a text
         
         //this is just gonna work for me...idk how to get the project directory
-        let dir = "/Users/yolimeydan/Documents/Github Repositories/FlightAttendantApp/FlightAttendantApp/\(fileName).txt"
+        let dir = "/Users/yolimeydan/Documents/Github Repositories/FlightAttendantApp/FlightAttendantApp/\(fileName).html"
         print(dir)
         //writing
         do {
